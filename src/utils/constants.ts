10815ca33d19ec09aac121d3a1e7f67391eb2862
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-10-04 19:21:23 +0800

    Refactor alphabetRegex

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ const NOTES_API =
     ? "http://localhost:3000"
     : "https://notes-api.adaptable.app";
 
-const alphabetRegex = /^[A-Za-z]+$/;
+const alphabetRegex = /^[a-zA-Z ]*$/;
 const minThreeCharactersRegex = /(.*[a-z]){3}/i;
 const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
