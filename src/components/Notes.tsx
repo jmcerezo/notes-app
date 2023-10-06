41d0255e7ecddd2316f6e0018005865107991259
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-10-06 19:13:54 +0800

    Fix UI for notes card

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -27,38 +27,37 @@ const Notes = () => {
     <>
       <div className="notes-container">
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            {notes.length ? (
-              notes.map((note: Note) => (
-                <Grid xs={12} sm={6} md={3} key={note._id}>
-                  <Card
-                    sx={{
-                      maxWidth: 345,
-                      borderRadius: "1rem",
-                    }}
-                  >
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {note.title}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {note.content}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small" variant="outlined">
-                        <EditOutlinedIcon />
-                      </Button>
-                      <Button size="small" variant="outlined">
-                        <DeleteOutlinedIcon />
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))
-            ) : (
-              <h1>Nothing to display.</h1>
-            )}
+          <Grid container spacing={2} justifyContent="center">
+            {notes.map((note: Note) => (
+              <Grid xs={12} sm={6} md={3} key={note._id}>
+                <Card
+                  sx={{
+                    maxWidth: 500,
+                    borderRadius: "1rem",
+                  }}
+                >
+                  <CardContent>
+                    <Typography color="text.secondary">
+                      {new Date(note.createdAt).toDateString()}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {note.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {note.content}
+                    </Typography>
+                  </CardContent>
+                  <CardActions sx={{ justifyContent: "center" }}>
+                    <Button size="small" variant="outlined">
+                      <EditOutlinedIcon />
+                    </Button>
+                    <Button size="small" variant="outlined">
+                      <DeleteOutlinedIcon />
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Box>
       </div>
